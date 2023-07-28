69ef12d53f48e03e4c58b95db6e3151ec064bc83
AnastasisZoyganelis <140797516+AnastasisZoyganelis@users.noreply.github.com>
2023-07-28 17:59:12 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import {Home,CreateBattle} from './page';
+import './index.css';
+import {GlobalContextProvider} from './context';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <GlobalContextProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create-battle" element={<CreateBattle />} />
+    </Routes>
+    </GlobalContextProvider>
+  </BrowserRouter>,
+);
